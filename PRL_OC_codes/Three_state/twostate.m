c5145b83a64ffcff1068db7130c40d6b35a50aec
Yikuan-PKU <yikuanzhang@outlook.com>
2024-11-28 21:53:35 +0800

    Add files via upload

---
+++ PRL_OC_codes/Three_state/twostate.m
@@ -0,0 +1,288 @@
+function [problem,guess] = twostate(tau,para)
+%myProblem - Template file for optimal control problem definition
+%
+%Syntax:  [problem,guess] = myProblem
+%
+% Outputs:
+%    problem - Structure with information on the optimal control problem
+%    guess   - Guess for state, control and multipliers.
+%
+% Other m-files required: none
+% MAT-files required: none
+%
+% Copyright (C) 2019 Yuanbo Nie, Omar Faqir, and Eric Kerrigan. All Rights Reserved.
+% The contribution of Paola Falugi, Eric Kerrigan and Eugene van Wyk for the work on ICLOCS Version 1 (2010) is kindly acknowledged.
+% This code is published under the MIT License.
+% Department of Aeronautics and Department of Electrical and Electronic Engineering,
+% Imperial College London London  England, UK 
+% ICLOCS (Imperial College London Optimal Control) Version 2.5 
+% 1 Aug 2019
+% iclocs@imperial.ac.uk
+K1=para(1);
+K2=para(2);
+K3=para(3);
+pi=zeros(1,2);
+pi(1)=para(4);
+pi(2)=para(5);
+pf=zeros(1,2);
+pf(1)=para(6);
+pf(2)=para(7);
+
+ri=zeros(1,3);
+rf=zeros(1,3);
+
+
+ri(1)=pi(2)/pi(1);
+ri(2)=pi(1)/(1-(pi(1)+pi(2)));
+ri(3)=1/(ri(1)*ri(2));
+rf(1)=pf(2)/pf(1);
+rf(2)=pf(1)/(1-(pf(1)+pf(2)));
+rf(3)=1/(rf(1)*rf(2));
+
+ki=zeros(1,6);
+kf=zeros(1,6);
+ki(1)=ri(1)*K1/(1+ri(1));
+ki(2)=K1/(1+ri(1));
+ki(4)=ri(2)*K2/(1+ri(2));
+ki(3)=K2/(1+ri(2));
+ki(5)=ri(3)*K3/(1+ri(3));
+ki(6)=K3/(1+ri(3));
+kf(1)=rf(1)*K1/(1+rf(1));
+kf(2)=K1/(1+rf(1));
+kf(4)=rf(2)*K2/(1+rf(2));
+kf(3)=K2/(1+rf(2));
+kf(5)=rf(3)*K3/(1+rf(3));
+kf(6)=K3/(1+rf(3));
+%------------- BEGIN CODE --------------
+% Plant model name, provide in the format of function handle
+InternalDynamics=@twostate_Dynamics_Internal; 
+SimDynamics=@twostate_Dynamics_Sim;
+
+% Analytic derivative files (optional), provide in the format of function handle
+
+% (optional) customized call back function
+%problem.callback=@callback_myProblem;
+
+% Settings file
+problem.settings=@settings_twostate;
+
+%Initial Time. t0<tf
+problem.time.t0_min=0;
+problem.time.t0_max=0;
+guess.t0=0;
+
+% Final time. Let tf_min=tf_max if tf is fixed.
+problem.time.tf_min=tau;     
+problem.time.tf_max=tau; 
+guess.tf=tau;
+
+% Parameters bounds. pl=< p <=pu
+problem.parameters.pl=[];
+problem.parameters.pu=[];
+guess.parameters=[];
+
+% Initial conditions for system.
+problem.states.x0=pi;
+
+% Initial conditions for system. Bounds if x0 is free s.t. x0l=< x0 <=x0u
+problem.states.x0l=pi; 
+problem.states.x0u=pi; 
+
+% State bounds. xl=< x <=xu
+problem.states.xl=[0,0];
+problem.states.xu=[1,1];
+
+% State rate bounds. xrl=< x_dot <=xru
+%problem.states.xrl=[]; 
+%problem.states.xru=[]; 
+
+% State error bounds
+problem.states.xErrorTol_local=[0.01,0.01]; 
+problem.states.xErrorTol_integral=[0.01,0.01]; 
+
+% State constraint error bounds
+problem.states.xConstraintTol=[0.01,0.01];
+%problem.states.xrConstraintTol=[0.1];
+
+% Terminal state bounds. xfl=< xf <=xfu
+problem.states.xfl=pf; 
+problem.states.xfu=pf;
+
+% Guess the state trajectories with [x0 ... xf]
+guess.time=[0 tau];
+guess.states(:,1)=[pi(1),pf(1)];
+guess.states(:,2)=[pi(2),pf(2)];
+
+% Number of control actions N 
+% Set problem.inputs.N=0 if N is equal to the number of integration steps.  
+% Note that the number of integration steps defined in settings.m has to be divisible 
+% by the  number of control actions N whenever it is not zero.
+problem.inputs.N=0;       
+      
+% Input bounds
+problem.inputs.ul=[0 0 0 0 0 0];
+problem.inputs.uu=[K1 K1 K2 K2 K3 K3];
+
+% Bounds on the first control action
+problem.inputs.u0l=[0 0 0 0 0 0];
+problem.inputs.u0u=[K1 K1 K2 K2 K3 K3];
+
+% Input rate bounds
+%problem.inputs.url=[]; 
+%problem.inputs.uru=[];
+% Input constraint error bounds
+problem.inputs.uConstraintTol=[0.01 0.01 0.01 0.01 0.01 0.01];
+%problem.inputs.urConstraintTol=[0.1 0.1];
+
+% Guess the input sequences with [u0 ... uf]
+guess.inputs(:,1)=[ki(1) kf(1)];
+guess.inputs(:,2)=[ki(2) kf(2)];
+guess.inputs(:,3)=[ki(3) kf(3)];
+guess.inputs(:,4)=[ki(4) kf(4)];
+guess.inputs(:,5)=[ki(5) kf(5)];
+guess.inputs(:,6)=[ki(6) kf(6)];
+% Choose the set-points if required
+problem.setpoints.states=[];
+problem.setpoints.inputs=[];
+% Path constraint function 
+problem.constraints.ng_eq=3; % number of quality constraints in format of g(x,u,p,t) == 0
+problem.constraints.gTol_eq=[0.01,0.01,0.01]; % equality cosntraint error bounds
+
+problem.constraints.gl=[]; % Lower ounds for inequality constraint function gl =< g(x,u,p,t) =< gu
+problem.constraints.gu=[]; % Upper ounds for inequality constraint function gl =< g(x,u,p,t) =< gu
+problem.constraints.gTol_neq=[]; % inequality constraint error bounds
+
+% OPTIONAL: define the time duration each constraint will be active, for
+% example (for ECH enabled in setings)
+
+
+% Bounds for boundary constraints bl =< b(x0,xf,u0,uf,p,t0,tf) =< bu
+problem.constraints.bl=[];
+problem.constraints.bu=[];
+problem.constraints.bTol=[]; 
+
+%problem.data.auxdata=[];
+
+problem.data.K1=para(1);
+problem.data.K2=para(2);
+problem.data.K3=para(3);
+problem.data.ki=ki;
+problem.data.kf=kf;
+% Get function handles and return to Main.m
+problem.data.InternalDynamics=InternalDynamics;
+problem.data.functionfg=@fg;
+problem.data.plantmodel = func2str(InternalDynamics);
+problem.functions={@L,@E,@f,@g,@avrc,@b};
+problem.sim.functions=SimDynamics;
+problem.sim.inputX=[];
+problem.sim.inputU=1:length(problem.inputs.ul);
+problem.functions_unscaled={@L_unscaled,@E_unscaled,@f_unscaled,@g_unscaled,@avrc,@b_unscaled};
+problem.data.functions_unscaled=problem.functions_unscaled;
+problem.data.ng_eq=problem.constraints.ng_eq;
+problem.constraintErrorTol=[problem.constraints.gTol_eq,problem.constraints.gTol_neq,problem.constraints.gTol_eq,problem.constraints.gTol_neq,problem.states.xConstraintTol,problem.states.xConstraintTol,problem.inputs.uConstraintTol,problem.inputs.uConstraintTol];
+
+%------------- END OF CODE --------------
+
+function stageCost=L_unscaled(x,xr,u,ur,p,t,vdat)
+
+% L_unscaled - Returns the stage cost.
+% The function must be vectorized and
+% xi, ui are column vectors taken as x(:,i) and u(:,i) (i denotes the i-th
+% variable)
+% 
+% Syntax:  stageCost = L(x,xr,u,ur,p,t,data)
+%
+% Inputs:
+%    x  - state vector
+%    xr - state reference
+%    u  - input
+%    ur - input reference
+%    p  - parameter
+%    t  - time
+%    data- structured variable containing the values of additional data used inside
+%          the function
+%
+% Output:
+%    stageCost - Scalar or vectorized stage cost
+%
+%  Remark: If the stagecost does not depend on variables it is necessary to multiply
+%          the assigned value by t in order to have right vector dimesion when called for the optimization. 
+%          Example: stageCost = 0*t;
+
+%------------- BEGIN CODE --------------
+
+%Define states and setpoints
+
+
+stageCost = (x(:,1).*u(:,1)-x(:,2).*u(:,2)).*log((x(:,1).*u(:,1))./(x(:,2).*u(:,2)))+(x(:,1).*u(:,3)-(1-x(:,1)-x(:,2)).*u(:,4)).*log((x(:,1).*u(:,3))./((1-x(:,1)-x(:,2)).*u(:,4)))+(x(:,2).*u(:,5)-(1-x(:,1)-x(:,2)).*u(:,6)).*log((x(:,2).*u(:,5))./((1-x(:,1)-x(:,2)).*u(:,6)));
+
+
+%------------- END OF CODE --------------
+
+
+function boundaryCost=E_unscaled(x0,xf,u0,uf,p,t0,tf,data) 
+
+% E_unscaled - Returns the boundary value cost
+%
+% Syntax:  boundaryCost=E(x0,xf,u0,uf,p,tf,data)
+%
+% Inputs:
+%    x0  - state at t=0
+%    xf  - state at t=tf
+%    u0  - input at t=0
+%    uf  - input at t=tf
+%    p   - parameter
+%    tf  - final time
+%    data- structured variable containing the values of additional data used inside
+%          the function
+%
+% Output:
+%    boundaryCost - Scalar boundary cost
+%
+%------------- BEGIN CODE --------------
+
+boundaryCost=0;
+
+%------------- END OF CODE --------------
+
+function bc=b_unscaled(x0,xf,u0,uf,p,t0,tf,data,varargin)
+
+% b_unscaled - Returns a column vector containing the evaluation of the boundary constraints: bl =< bf(x0,xf,u0,uf,p,t0,tf) =< bu
+%
+% Syntax:  bc=b(x0,xf,u0,uf,p,tf,data)
+%
+% Inputs:
+%    x0  - state at t=0
+%    xf  - state at t=tf
+%    u0  - input at t=0
+%    uf  - input at t=tf
+%    p   - parameter
+%    tf  - final time
+%    data- structured variable containing the values of additional data used inside
+%          the function
+%
+%          
+% Output:
+%    bc - column vector containing the evaluation of the boundary function 
+%
+% Leave it here
+varargin=varargin{1};
+%------------- BEGIN CODE --------------
+bc=[];
+
+%------------- END OF CODE --------------
+% When adpative time interval add constraint on time
+if length(varargin)==2
+    options=varargin{1};
+    t_segment=varargin{2};
+    if (strcmp(options.discretization,'hpLGR'))  && options.adaptseg==1 
+        if size(t_segment,1)>size(t_segment,2)
+            bc=[bc;diff(t_segment)];
+        else
+            bc=[bc;diff(t_segment)];
+        end
+    end
+end
+
+%------------- END OF CODE --------------
+
---
+++ PRL_OC_codes/Three_state/twostate_Dynamics_Internal.m
@@ -0,0 +1,51 @@
+function [dx,g_eq] = twostate_Dynamics_Internal(x,u,p,t,vdat)
+% Template for specifying the dynamics for internal model 
+%
+% Syntax:  
+%          [dx] = myProblem_Dynamics_Internal(x,u,p,t,vdat)	(Dynamics Only)
+%          [dx,g_eq] = myProblem_Dynamics_Internal(x,u,p,t,vdat)   (Dynamics and Eqaulity Path Constraints)
+%          [dx,g_neq] = myProblem_Dynamics_Internal(x,u,p,t,vdat)   (Dynamics and Inqaulity Path Constraints)
+%          [dx,g_eq,g_neq] = myProblem_Dynamics_Internal(x,u,p,t,vdat)   (Dynamics, Equality and Ineqaulity Path Constraints)
+% 
+% Inputs:
+%    x  - state vector
+%    u  - input
+%    p  - parameter
+%    t  - time
+%    data - structured variable containing the values of additional data used inside
+%          the function%      
+% Output:
+%    dx - time derivative of x
+%    g_eq - constraint function for equality constraints
+%    g_neq - constraint function for inequality constraints
+%
+% Copyright (C) 2019 Yuanbo Nie, Omar Faqir, and Eric Kerrigan. All Rights Reserved.
+% The contribution of Paola Falugi, Eric Kerrigan and Eugene van Wyk for the work on ICLOCS Version 1 (2010) is kindly acknowledged.
+% This code is published under the MIT License.
+% Department of Aeronautics and Department of Electrical and Electronic Engineering,
+% Imperial College London London  England, UK 
+% ICLOCS (Imperial College London Optimal Control) Version 2.5 
+% 1 Aug 2019
+% iclocs@imperial.ac.uk
+
+%------------- BEGIN CODE --------------
+%Stored data
+
+%Define states
+x1 = x(:,1);
+x2 = x(:,2);
+%Define inputs
+k12 = u(:,1);
+k21 = u(:,2);
+k13 = u(:,3);
+k31 = u(:,4);
+k23 = u(:,5);
+k32 = u(:,6);
+%Define ODE right-hand side
+
+dx(:,1) = x2.*(k21-k31)-x1.*(k12+k31+k13)+k31;
+dx(:,2) = x1.*(k12-k32)-x2.*(k21+k23+k32)+k32;
+g_eq(:,1)=k12+k21-vdat.K1;
+g_eq(:,2)=k13+k31-vdat.K2;
+g_eq(:,3)=k23+k32-vdat.K3;
+%------------- END OF CODE --------------
